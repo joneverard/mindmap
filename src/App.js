2b6c03bf9d4cbe443531bf9807d4fe8ff8f212ba
Jon <jonster991@gmail.com>
2017-10-13 16:21:58 +0100

    initial commit with create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Ribbon from 'components/ribbon';
+
+// this is where to build the app...
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ribbon.js
@@ -0,0 +1,4 @@
+import { Component } from 'react';
+// will need to connect this to an action creator at some point.
+// import action from '../blahhhh actions.index.js'
+
